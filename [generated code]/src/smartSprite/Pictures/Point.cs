a83f312e30e3529976307632ae41d59db070001c
MarceloGodoy <mgodoy_desenv@hotmail.com>
2015-12-28 23:22:36 -0200

    Inclusão no controle de versão

---
+++ [generated code]/src/smartSprite/Pictures/Point.cs
@@ -0,0 +1,30 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace smartSprite.Pictures{
+	/// <summary>
+	/// Represents a cartesyan coordinate
+	/// </summary>
+	public class Point {
+
+		/// <summary>
+		/// Represents a cartesyan coordinate
+		/// </summary>
+		public Point() {
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public float X;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public float Y;
+
+	}
+}
---
+++ [generated code]/src/smartSprite/Unity/MetaSprite.cs
@@ -0,0 +1,38 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace smartSprite.Unity{
+	/// <summary>
+	/// Represents a meta class of sprites
+	/// </summary>
+	public class MetaSprite {
+
+		/// <summary>
+		/// Represents a meta class of sprites
+		/// </summary>
+		public MetaSprite() {
+		}
+
+		/// <summary>
+		/// The piece to add meta sprite
+		/// </summary>
+		private Piece _piece;
+
+		/// <param name="piece"></param>
+		public void MetaSprite(Piece piece) {
+			// TODO implement here
+		}
+
+		/// <summary>
+		/// Generates a meta file in specified path.
+		/// </summary>
+		/// <param name="fullPath"></param>
+		public void Generate(String fullPath) {
+			// TODO implement here
+		}
+
+	}
+}
