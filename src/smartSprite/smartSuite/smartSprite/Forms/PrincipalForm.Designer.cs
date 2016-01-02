0e3d0d1adb206307f661f1024b50c6a43825cf85
MarceloGodoy <mgodoy_desenv@hotmail.com>
2016-01-02 16:30:47 -0200

    Preparação primeiro GUI

---
+++ src/smartSprite/smartSuite/smartSprite/Forms/PrincipalForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace smartSprite.Forms
+{
+    partial class PrincipalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlSourceFolder = new System.Windows.Forms.Panel();
+            this.txtSourceFolder = new System.Windows.Forms.TextBox();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.pnlSourceFolder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 386);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(903, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(903, 386);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblVersion);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(897, 44);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(7, 0);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(51, 13);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "Indefined";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::smartSprite.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(494, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.27648F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.72353F));
+            this.tableLayoutPanel2.Controls.Add(this.pnlSourceFolder, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 53);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(897, 330);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // pnlSourceFolder
+            // 
+            this.pnlSourceFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.pnlSourceFolder.Controls.Add(this.txtSourceFolder);
+            this.pnlSourceFolder.Controls.Add(this.btnOpenFolder);
+            this.pnlSourceFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlSourceFolder.Location = new System.Drawing.Point(3, 3);
+            this.pnlSourceFolder.Name = "pnlSourceFolder";
+            this.pnlSourceFolder.Size = new System.Drawing.Size(140, 324);
+            this.pnlSourceFolder.TabIndex = 0;
+            // 
+            // txtSourceFolder
+            // 
+            this.txtSourceFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSourceFolder.Location = new System.Drawing.Point(7, 33);
+            this.txtSourceFolder.Multiline = true;
+            this.txtSourceFolder.Name = "txtSourceFolder";
+            this.txtSourceFolder.Size = new System.Drawing.Size(120, 44);
+            this.txtSourceFolder.TabIndex = 2;
+            this.txtSourceFolder.Leave += new System.EventHandler(this.txtSourceFolder_Leave);
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(7, 3);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(37, 24);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "...";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select a source folder";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // PrincipalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 408);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "PrincipalForm";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.pnlSourceFolder.ResumeLayout(false);
+            this.pnlSourceFolder.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel pnlSourceFolder;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtSourceFolder;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblVersion;
+    }
+}
---
+++ src/smartSprite/smartSuite/smartSprite/Forms/PrincipalForm.cs
@@ -0,0 +1,363 @@
+﻿using smartSprite.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace smartSprite.Forms
+{
+    public partial class PrincipalForm : Form
+    {
+        public PrincipalForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Refresh the tree view list
+        /// </summary>
+        private void RefreshTreeViewList(String sourceFolder, TreeNodeCollection nodes)
+        {
+            #region Entries validation
+
+            if (String.IsNullOrEmpty(sourceFolder))
+            {
+                throw new ArgumentNullException("sourceFolder");
+            }
+            if (nodes == null)
+            {
+                throw new ArgumentNullException("nodes");
+            }
+
+            #endregion
+
+            this.ShowRunningResult(false);
+
+            nodes.Clear();
+
+            var directoryList = Directory.GetDirectories(sourceFolder);
+            var directoryNodeList = ConvertNodeList(directoryList, false);
+
+            /*
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.AfterCheck += TreeView1_AfterCheck;
+            this.treeView1.Nodes.AddRange(directoryNodeList.ToArray());
+            this.treeView1.ExpandAll();
+            */
+        }
+
+        /// <summary>
+        /// Converts the list to list of treenodes.
+        /// </summary>
+        private List<TreeNode> ConvertNodeList(string[] uriList, bool handleUriAsFiles)
+        {
+            #region Entries validation
+
+            if (uriList == null)
+            {
+                throw new ArgumentNullException("directoryList");
+            }
+
+            #endregion
+
+            List<TreeNode> returnList = new List<TreeNode>();
+
+            foreach (var uriItem in uriList)
+            {
+                try
+                {
+                    TreeNode treeNode = new TreeNode();
+                    treeNode.Tag = uriItem;
+                    treeNode.Text = Path.GetFileName(uriItem);
+
+                    // Adding files
+                    if (!handleUriAsFiles)
+                    {
+                        // Adding sub directories
+                        treeNode.Nodes.AddRange(
+                            ConvertNodeList(
+                                Directory.GetDirectories(uriItem),
+                                false).ToArray());
+
+                        treeNode.Nodes.AddRange(
+                            ConvertNodeList(
+                                GetExceptSmartMap(
+                                    Directory.GetFiles(uriItem, "*.png")),
+                                true).ToArray());
+
+                        treeNode.Nodes.AddRange(
+                            ConvertNodeList(
+                                GetExceptSmartMap(
+                                    Directory.GetFiles(uriItem, "*.bmp")),
+                                true).ToArray());
+                    }
+                    // Handling files
+                    else
+                    {
+                        treeNode.Checked = true;
+                    }
+
+                    returnList.Add(treeNode);
+                }
+                catch (IOException)
+                {
+                    // Errors of this kind in this location cannot turns around the normal flow of algoritmn
+                    continue;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // Errors of this kind in this location cannot turns around the normal flow of algoritmn
+                    continue;
+                }
+            }
+
+            return returnList;
+        }
+
+        /// <summary>
+        /// Get a list informed, except for the smartMaps files.
+        /// </summary>
+        /// <param name="fileList"></param>
+        /// <returns></returns>
+        private string[] GetExceptSmartMap(string[] fileList)
+        {
+            #region Entries validation
+
+            if (fileList == null)
+            {
+                throw new ArgumentNullException("fileList");
+            }
+
+            #endregion
+
+            List<String> fileListTemp = new List<string>(fileList);
+            return fileListTemp.FindAll(delegate (String item)
+            {
+                return !item.Contains(".smartMap.");
+            }).ToArray();
+        }
+
+        /// <summary>
+        /// Shows the runnnig result
+        /// </summary>
+        private void ShowRunningResult(bool visibility)
+        {
+        }
+
+        /// <summary>
+        /// Gets the list of selected nodes
+        /// </summary>
+        /// <param name="nodes"></param>
+        /// <returns></returns>
+        private String[] GetSelectedNodesString(TreeNodeCollection nodes)
+        {
+            var selectedNodeList = this.GetSelectedNodes(nodes);
+
+            List<String> retunedList = new List<string>();
+
+            foreach (var item in selectedNodeList)
+            {
+                retunedList.Add((String)item.Tag);
+            }
+
+            return retunedList.ToArray();
+        }
+
+        /// <summary>
+        /// Gets the list of selected nodes
+        /// </summary>
+        /// <param name="nodes"></param>
+        /// <returns></returns>
+        private List<TreeNode> GetSelectedNodes(TreeNodeCollection nodes)
+        {
+            #region Entries validation
+
+            if (nodes == null)
+            {
+                throw new ArgumentNullException("nodes");
+            }
+
+            #endregion
+
+            List<TreeNode> returnValueList = new List<TreeNode>();
+
+            foreach (var item in nodes)
+            {
+                TreeNode treeNode = (TreeNode)item;
+
+                #region Entries validation
+
+                if (treeNode.Nodes.Count > 0)
+                {
+                    returnValueList.AddRange(
+                        this.GetSelectedNodes(treeNode.Nodes));
+
+                    continue;
+                }
+
+                #endregion
+
+                if (treeNode.Checked)
+                {
+                    returnValueList.Add(treeNode);
+                }
+            }
+
+            return returnValueList;
+        }
+
+        /// <summary>
+        /// Loads settings
+        /// </summary>
+        private void LoadSettings()
+        {
+            if (!String.IsNullOrEmpty(Settings.Default.lastSourceFolder))
+            {
+                this.txtSourceFolder.Text = Settings.Default.lastSourceFolder;
+                this.folderBrowserDialog1.SelectedPath = this.txtSourceFolder.Text.Trim();
+            }
+            else
+            {
+                this.txtSourceFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            }
+
+            this.lblVersion.Text = this.GetType().Assembly.GetName().Version.ToString() + "(Alpha)";
+
+            this.RefreshForm();
+        }
+
+        /// <summary>
+        /// Refreshes the form
+        /// </summary>
+        private void RefreshForm()
+        {
+            if (!Directory.Exists(this.txtSourceFolder.Text))
+            {
+                return;
+            }
+
+            try
+            {
+                this.Interrupt();
+
+                this.folderBrowserDialog1.SelectedPath = this.txtSourceFolder.Text.Trim();
+                Settings.Default.lastSourceFolder = this.txtSourceFolder.Text.Trim();
+                Settings.Default.Save();
+            }
+            finally
+            {
+                this.Resume();
+            }
+        }
+
+        /// <summary>
+        /// Interrupts the user interface
+        /// </summary>
+        private void Interrupt()
+        {
+            this.EnableForm(false);
+        }
+
+        /// <summary>
+        /// Resumes the user interface
+        /// </summary>
+        private void Resume()
+        {
+            this.EnableForm(true);
+        }
+
+        /// <summary>
+        /// Enables / disables form
+        /// </summary>
+        /// <param name="enabled"></param>
+        private void EnableForm(bool enabled)
+        {
+            if (enabled)
+            {
+                this.Cursor = Cursors.Default;
+            }
+            else
+            {
+                this.Cursor = Cursors.WaitCursor;
+            }
+
+            this.pnlSourceFolder.Enabled = enabled;
+        }
+
+        #region Events
+
+        /// <summary>
+        /// Loads the form
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            this.LoadSettings();
+
+            this.FormClosed += PrincipalForm_FormClosed;
+        }
+
+        /// <summary>
+        /// Occurs when the form has closed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PrincipalForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Handles the click on check boxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TreeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Nodes.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var item in e.Node.Nodes)
+            {
+                ((TreeNode)item).Checked = e.Node.Checked;
+            }
+        }
+
+        /// <summary>
+        /// Open folders
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1.ShowDialog();
+            if (this.folderBrowserDialog1.SelectedPath != null)
+            {
+                this.txtSourceFolder.Text = this.folderBrowserDialog1.SelectedPath;
+                this.RefreshForm();
+            }
+        }
+
+        /// <summary>
+        /// Ocurrs when the user leaves the focus of source folder
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtSourceFolder_Leave(object sender, EventArgs e)
+        {
+            this.RefreshForm();
+        }
+
+        #endregion
+    }
+}
