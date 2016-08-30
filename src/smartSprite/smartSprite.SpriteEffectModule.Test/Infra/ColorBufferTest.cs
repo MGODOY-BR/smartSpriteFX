1ca0f20cefcc10b91399a0e5b5838febf61119bc
MarceloGodoy <mgodoy_desenv@hotmail.com>
2016-08-30 18:02:39 -0300

    Teste de ColorBuffer concluído

--- src/smartSprite/smartSprite.SpriteEffectModule.Test/Infra/ColorBufferTest.cs
+++ src/smartSprite/smartSprite.SpriteEffectModule.Test/Infra/ColorBufferTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using smartSuite.smartSprite.Effects.Infra;
+using smartSprite.Pictures.ColorPattern;
 
 namespace smartSprite.SpriteEffectModule.Test.Infra
 {
@@ -157,6 +158,99 @@ namespace smartSprite.SpriteEffectModule.Test.Infra
             #region Validating the evidences
 
             Assert.AreEqual(4, evidence);
+
+            #endregion
+        }
+
+        [TestMethod]
+        public void GetSimilarColorTest()
+        {
+            #region Scenario setup
+
+            ColorBuffer test = new ColorBuffer(4);
+            for (int i = 0; i < 255; i += 255 / 4)
+            {
+                test.Register(
+                    System.Drawing.Color.FromArgb(255, 255, i));
+            }
+
+            #endregion
+
+            #region Running the tested operation
+
+            var evidenceA =
+                test.GetSimilarColor(
+                    System.Drawing.Color.FromArgb(255, 225, 64));
+
+            var evidenceB =
+                test.GetSimilarColor(
+                    System.Drawing.Color.FromArgb(255, 225, 65));
+
+            var evidenceC =
+                test.GetSimilarColor(
+                    System.Drawing.Color.FromArgb(255, 225, 80));
+
+            var evidenceD =
+                test.GetSimilarColor(
+                    System.Drawing.Color.FromArgb(255, 225, 100));
+
+            #endregion
+
+            #region Getting the evidences
+
+            #endregion
+
+            #region Validating the evidences
+
+            ColorEqualityComparer comparer = new ColorEqualityComparer(0);
+
+            Assert.IsTrue(
+                comparer.Equals(evidenceA, evidenceB));
+
+            Assert.IsTrue(
+                comparer.Equals(evidenceA, evidenceC));
+
+            Assert.IsTrue(
+                comparer.Equals(evidenceA, evidenceD));
+
+            Assert.IsTrue(
+                comparer.Equals(evidenceB, evidenceC));
+
+            Assert.IsTrue(
+                comparer.Equals(evidenceA, evidenceC));
+
+            #endregion
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            #region Scenario setup
+
+            ColorBuffer test = new ColorBuffer(4);
+            for (int i = 0; i < 255; i += 255 / 4)
+            {
+                test.Register(
+                    System.Drawing.Color.FromArgb(255, 255, i));
+            }
+
+            #endregion
+
+            #region Running the tested operation
+
+            test.Clear();
+
+            #endregion
+
+            #region Getting the evidences
+
+            var evidence = test.Count();
+
+            #endregion
+
+            #region Validating the evidences
+
+            Assert.AreEqual(0, evidence);
 
             #endregion
         }
