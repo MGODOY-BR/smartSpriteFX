ab60d691aacc1dfe170f381be98d264f38e48ff9
MarceloGodoy <mgodoy_desenv@hotmail.com>
2016-10-09 16:53:47 -0300

    ConfigurationPanel in progress

---
+++ src/smartSprite/smartSprite.SpriteEffectModule/Effects/Infra/UI/Configuratons/DefaultConfigurationPanel.cs
@@ -0,0 +1,22 @@
+﻿using smartSuite.smartSprite.Effects.Infra.UI.Configuratons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using smartSuite.smartSprite.Effects.Filters;
+using System.Windows.Forms;
+
+namespace smartSprite.SpriteEffectModule.Effects.Infra.UI.Configuratons
+{
+    /// <summary>
+    /// Offers an default configuration Panel with no implementation remarkable
+    /// </summary>
+    public class DefaultConfigurationPanel : IConfigurationPanel
+    {
+        public Panel GetPanel(IEffectFilter effectFilter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
