a4fcb13a19077278c28ccc76586d779aacbbeb97
MarceloGodoy <mgodoy_desenv@hotmail.com>
2016-11-02 12:35:53 -0200

    Revisão da aparência de EffectControl

--- src/smartSprite/smartSuite/smartSprite/Forms/AnimationForm.Designer.cs
+++ src/smartSprite/smartSuite/smartSprite/Forms/AnimationForm.Designer.cs
@@ -36,7 +36,6 @@
             this.previewBoard = new System.Windows.Forms.PictureBox();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.pnlFilterPanel = new System.Windows.Forms.Panel();
-            this.pnlApply = new System.Windows.Forms.Panel();
             this.btnPreview = new System.Windows.Forms.Button();
             this.btnApplyAll = new System.Windows.Forms.Button();
             this.splitContainer3 = new System.Windows.Forms.SplitContainer();
@@ -50,6 +49,8 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panelTool = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.topPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -61,7 +62,6 @@
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
-            this.pnlApply.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
             this.splitContainer3.Panel1.SuspendLayout();
             this.splitContainer3.Panel2.SuspendLayout();
@@ -71,13 +71,15 @@
             this.panelToolContainer.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 455);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 496);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(806, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(928, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -87,7 +89,7 @@
             this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.topPanel.Location = new System.Drawing.Point(0, 36);
             this.topPanel.Name = "topPanel";
-            this.topPanel.Size = new System.Drawing.Size(806, 108);
+            this.topPanel.Size = new System.Drawing.Size(928, 108);
             this.topPanel.TabIndex = 3;
             // 
             // frameBox
@@ -96,7 +98,7 @@
             this.frameBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.frameBox.Location = new System.Drawing.Point(0, 0);
             this.frameBox.Name = "frameBox";
-            this.frameBox.Size = new System.Drawing.Size(806, 108);
+            this.frameBox.Size = new System.Drawing.Size(928, 108);
             this.frameBox.TabIndex = 0;
             this.frameBox.TabStop = false;
             this.frameBox.Text = "Frames";
@@ -114,8 +116,8 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(627, 311);
-            this.splitContainer1.SplitterDistance = 406;
+            this.splitContainer1.Size = new System.Drawing.Size(749, 352);
+            this.splitContainer1.SplitterDistance = 461;
             this.splitContainer1.TabIndex = 5;
             // 
             // panel1
@@ -129,7 +131,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(406, 311);
+            this.panel1.Size = new System.Drawing.Size(461, 352);
             this.panel1.TabIndex = 0;
             // 
             // previewBoard
@@ -151,46 +153,34 @@
             // 
             // splitContainer2.Panel1
             // 
-            this.splitContainer2.Panel1.Controls.Add(this.pnlFilterPanel);
-            this.splitContainer2.Panel1.Controls.Add(this.pnlApply);
+            this.splitContainer2.Panel1.BackColor = System.Drawing.Color.Brown;
+            this.splitContainer2.Panel1.Controls.Add(this.panel2);
             // 
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
             this.splitContainer2.Panel2.Controls.Add(this.pnlSettingTop);
-            this.splitContainer2.Size = new System.Drawing.Size(217, 311);
-            this.splitContainer2.SplitterDistance = 101;
+            this.splitContainer2.Size = new System.Drawing.Size(284, 352);
+            this.splitContainer2.SplitterDistance = 156;
             this.splitContainer2.TabIndex = 0;
             // 
             // pnlFilterPanel
             // 
             this.pnlFilterPanel.AutoScroll = true;
-            this.pnlFilterPanel.AutoSize = true;
             this.pnlFilterPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.pnlFilterPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pnlFilterPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlFilterPanel.Location = new System.Drawing.Point(0, 28);
+            this.pnlFilterPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlFilterPanel.Location = new System.Drawing.Point(113, 0);
             this.pnlFilterPanel.Name = "pnlFilterPanel";
-            this.pnlFilterPanel.Size = new System.Drawing.Size(217, 73);
+            this.pnlFilterPanel.Size = new System.Drawing.Size(140, 154);
             this.pnlFilterPanel.TabIndex = 0;
-            // 
-            // pnlApply
-            // 
-            this.pnlApply.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pnlApply.Controls.Add(this.btnPreview);
-            this.pnlApply.Controls.Add(this.btnApplyAll);
-            this.pnlApply.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlApply.Location = new System.Drawing.Point(0, 0);
-            this.pnlApply.Name = "pnlApply";
-            this.pnlApply.Size = new System.Drawing.Size(217, 28);
-            this.pnlApply.TabIndex = 1;
             // 
             // btnPreview
             // 
-            this.btnPreview.Location = new System.Drawing.Point(100, 2);
+            this.btnPreview.Location = new System.Drawing.Point(6, 19);
             this.btnPreview.Name = "btnPreview";
-            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.Size = new System.Drawing.Size(93, 23);
             this.btnPreview.TabIndex = 1;
             this.btnPreview.Text = "PREVIEW";
             this.btnPreview.UseVisualStyleBackColor = true;
@@ -199,9 +189,9 @@
             // btnApplyAll
             // 
             this.btnApplyAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnApplyAll.Location = new System.Drawing.Point(3, 2);
+            this.btnApplyAll.Location = new System.Drawing.Point(6, 74);
             this.btnApplyAll.Name = "btnApplyAll";
-            this.btnApplyAll.Size = new System.Drawing.Size(91, 23);
+            this.btnApplyAll.Size = new System.Drawing.Size(93, 31);
             this.btnApplyAll.TabIndex = 0;
             this.btnApplyAll.Text = "APPLY ALL";
             this.btnApplyAll.UseVisualStyleBackColor = true;
@@ -221,8 +211,8 @@
             // splitContainer3.Panel2
             // 
             this.splitContainer3.Panel2.Controls.Add(this.pnlSettingsMain);
-            this.splitContainer3.Size = new System.Drawing.Size(217, 173);
-            this.splitContainer3.SplitterDistance = 65;
+            this.splitContainer3.Size = new System.Drawing.Size(284, 159);
+            this.splitContainer3.SplitterDistance = 59;
             this.splitContainer3.TabIndex = 2;
             // 
             // txtSettingsDescription
@@ -236,7 +226,7 @@
             this.txtSettingsDescription.Name = "txtSettingsDescription";
             this.txtSettingsDescription.ReadOnly = true;
             this.txtSettingsDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtSettingsDescription.Size = new System.Drawing.Size(217, 65);
+            this.txtSettingsDescription.Size = new System.Drawing.Size(284, 59);
             this.txtSettingsDescription.TabIndex = 0;
             // 
             // pnlSettingsMain
@@ -248,7 +238,7 @@
             this.pnlSettingsMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlSettingsMain.Location = new System.Drawing.Point(0, 0);
             this.pnlSettingsMain.Name = "pnlSettingsMain";
-            this.pnlSettingsMain.Size = new System.Drawing.Size(217, 104);
+            this.pnlSettingsMain.Size = new System.Drawing.Size(284, 96);
             this.pnlSettingsMain.TabIndex = 1;
             // 
             // pnlSettingTop
@@ -258,7 +248,7 @@
             this.pnlSettingTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlSettingTop.Location = new System.Drawing.Point(0, 0);
             this.pnlSettingTop.Name = "pnlSettingTop";
-            this.pnlSettingTop.Size = new System.Drawing.Size(217, 33);
+            this.pnlSettingTop.Size = new System.Drawing.Size(284, 33);
             this.pnlSettingTop.TabIndex = 0;
             // 
             // label1
@@ -277,7 +267,7 @@
             this.panelContainer.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelContainer.Location = new System.Drawing.Point(0, 144);
             this.panelContainer.Name = "panelContainer";
-            this.panelContainer.Size = new System.Drawing.Size(179, 311);
+            this.panelContainer.Size = new System.Drawing.Size(179, 352);
             this.panelContainer.TabIndex = 4;
             // 
             // panelToolContainer
@@ -292,7 +282,7 @@
             this.panelToolContainer.RowCount = 2;
             this.panelToolContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.44373F));
             this.panelToolContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.55627F));
-            this.panelToolContainer.Size = new System.Drawing.Size(179, 311);
+            this.panelToolContainer.Size = new System.Drawing.Size(179, 352);
             this.panelToolContainer.TabIndex = 0;
             // 
             // panelBrowser
@@ -300,16 +290,16 @@
             this.panelBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelBrowser.Location = new System.Drawing.Point(3, 3);
             this.panelBrowser.Name = "panelBrowser";
-            this.panelBrowser.Size = new System.Drawing.Size(173, 125);
+            this.panelBrowser.Size = new System.Drawing.Size(173, 143);
             this.panelBrowser.TabIndex = 0;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.panelTool);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(3, 134);
+            this.groupBox1.Location = new System.Drawing.Point(3, 152);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(173, 174);
+            this.groupBox1.Size = new System.Drawing.Size(173, 197);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Toolbox";
@@ -319,7 +309,7 @@
             this.panelTool.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelTool.Location = new System.Drawing.Point(3, 16);
             this.panelTool.Name = "panelTool";
-            this.panelTool.Size = new System.Drawing.Size(167, 155);
+            this.panelTool.Size = new System.Drawing.Size(167, 178);
             this.panelTool.TabIndex = 1;
             // 
             // pictureBox1
@@ -329,15 +319,36 @@
             this.pictureBox1.Image = global::smartSprite.Properties.Resources.logo;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(806, 36);
+            this.pictureBox1.Size = new System.Drawing.Size(928, 36);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Controls.Add(this.pnlFilterPanel);
+            this.panel2.Location = new System.Drawing.Point(37, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(253, 154);
+            this.panel2.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnPreview);
+            this.groupBox2.Controls.Add(this.btnApplyAll);
+            this.groupBox2.Location = new System.Drawing.Point(3, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(104, 140);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
             // 
             // AnimationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(806, 477);
+            this.ClientSize = new System.Drawing.Size(928, 518);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panelContainer);
             this.Controls.Add(this.topPanel);
@@ -355,11 +366,9 @@
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.previewBoard)).EndInit();
             this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel1.PerformLayout();
             this.splitContainer2.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
-            this.pnlApply.ResumeLayout(false);
             this.splitContainer3.Panel1.ResumeLayout(false);
             this.splitContainer3.Panel1.PerformLayout();
             this.splitContainer3.Panel2.ResumeLayout(false);
@@ -372,6 +381,8 @@
             this.panelToolContainer.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -388,7 +399,6 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.Panel pnlFilterPanel;
-        private System.Windows.Forms.Panel pnlApply;
         private System.Windows.Forms.Panel pnlSettingTop;
         private System.Windows.Forms.Panel pnlSettingsMain;
         private System.Windows.Forms.Label label1;
@@ -401,5 +411,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnApplyAll;
         private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
--- src/smartSprite/smartSuite/smartSprite/Forms/Controls/Animations/Effects/EffectControl.Designer.cs
+++ src/smartSprite/smartSuite/smartSprite/Forms/Controls/Animations/Effects/EffectControl.Designer.cs
@@ -31,9 +31,9 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnExclude = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
             this.btnDown = new System.Windows.Forms.Button();
             this.btnUp = new System.Windows.Forms.Button();
-            this.btnSettings = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,18 +44,20 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(33, 3);
+            this.panel1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 23);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(176, 47);
+            this.panel1.Size = new System.Drawing.Size(114, 52);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Open Sans", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(176, 47);
+            this.label1.Size = new System.Drawing.Size(114, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -70,14 +72,24 @@
             this.btnExclude.UseVisualStyleBackColor = true;
             this.btnExclude.Click += new System.EventHandler(this.btnExclude_Click);
             // 
+            // btnSettings
+            // 
+            this.btnSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSettings.Location = new System.Drawing.Point(3, 71);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(24, 23);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.Text = "...";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
             // btnDown
             // 
-            this.btnDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnDown.Image = global::smartSprite.Properties.Resources.down;
-            this.btnDown.Location = new System.Drawing.Point(215, 27);
+            this.btnDown.Location = new System.Drawing.Point(78, 76);
             this.btnDown.Name = "btnDown";
-            this.btnDown.Size = new System.Drawing.Size(50, 23);
+            this.btnDown.Size = new System.Drawing.Size(27, 13);
             this.btnDown.TabIndex = 3;
             this.btnDown.UseVisualStyleBackColor = true;
             this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
@@ -86,23 +98,12 @@
             // 
             this.btnUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnUp.Image = global::smartSprite.Properties.Resources.up;
-            this.btnUp.Location = new System.Drawing.Point(215, 3);
+            this.btnUp.Location = new System.Drawing.Point(78, 6);
             this.btnUp.Name = "btnUp";
-            this.btnUp.Size = new System.Drawing.Size(50, 23);
+            this.btnUp.Size = new System.Drawing.Size(27, 14);
             this.btnUp.TabIndex = 1;
             this.btnUp.UseVisualStyleBackColor = true;
             this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
-            // 
-            // btnSettings
-            // 
-            this.btnSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnSettings.Location = new System.Drawing.Point(3, 27);
-            this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(24, 23);
-            this.btnSettings.TabIndex = 1;
-            this.btnSettings.Text = "...";
-            this.btnSettings.UseVisualStyleBackColor = true;
-            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
             // 
             // EffectControl
             // 
@@ -111,7 +112,6 @@
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.BackgroundImage = global::smartSprite.Properties.Resources.EffectControl;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.btnDown);
             this.Controls.Add(this.btnExclude);
@@ -120,7 +120,7 @@
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "EffectControl";
-            this.Size = new System.Drawing.Size(268, 53);
+            this.Size = new System.Drawing.Size(117, 97);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
