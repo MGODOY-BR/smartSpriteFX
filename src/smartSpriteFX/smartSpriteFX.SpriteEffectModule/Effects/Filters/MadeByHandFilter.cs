c3fbad648eaaf6cc2e0b4051d546d3bfeabfdf88
MarceloGodoy <mgodoy_desenv@hotmail.com>
2017-02-28 19:46:30 -0300

    Filtro Made By Hand

--- src/smartSpriteFX/smartSpriteFX.SpriteEffectModule/Effects/Filters/MadeByHandFilter.cs
+++ src/smartSpriteFX/smartSpriteFX.SpriteEffectModule/Effects/Filters/MadeByHandFilter.cs
@@ -8,6 +8,10 @@ using smartSuite.smartSpriteFX.Effects.Infra.UI.Configuratons;
 using smartSuite.smartSpriteFX.Pictures;
 using smartSuite.smartSpriteFX.Pictures.PixelPatterns;
 using smartSuite.smartSpriteFX.PictureEngine.Pictures;
+using System.Drawing;
+using smartSuite.smartSpriteFX.Pictures.ColorPattern;
+using smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters.UI;
+using smartSuite.smartSpriteFX.Effects.Infra;
 
 namespace smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters
 {
@@ -21,20 +25,65 @@ namespace smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters
             List<PointInfo>[] columnList =
                 frame.GetColumns();
 
-            List<PointInfo>[] rowList =
+            List<PointInfo>[] lineList =
                 frame.GetLines();
+
+            ColorEqualityComparer comparer = new ColorEqualityComparer();
+
+            // Column handling
+            Color comparing = Color.Transparent;
+            foreach (var columnItem in columnList)
+            {
+                foreach (var pointItem in columnItem)
+                {
+                    Color currentColor = pointItem.Color;
+                    if (!comparer.LooksLikeBySensibility3(comparing, currentColor))
+                    {
+                        pointItem.Color = Color.Black;
+                    }
+                    comparing = currentColor;
+                }
+            }
+
+            // Line handling
+            comparing = Color.Transparent;
+            foreach (var lineItem in lineList)
+            {
+                foreach (var pointItem in lineItem)
+                {
+                    Color currentColor = pointItem.Color;
+                    if (!comparer.LooksLikeBySensibility3(comparing, currentColor))
+                    {
+                        pointItem.Color = Color.Black;
+                    }
+                    comparing = currentColor;
+                }
+            }
 
             return true;
         }
 
         public override void Reset()
         {
-            throw new NotImplementedException();
         }
 
         public override IConfigurationPanel ShowConfigurationPanel()
         {
-            throw new NotImplementedException();
+            return new NoneConfigurationPanelControl();
+        }
+
+        /// <summary>
+        /// Gets the identification
+        /// </summary>
+        /// <returns></returns>
+        public override Identification GetIdentification()
+        {
+            var identification = base.GetIdentification();
+            identification.SetName("Made By Hand");
+            identification.SetDescription("Represents a filter used to emulate draw made by hand");
+            identification.SetGroup("Art");
+
+            return identification;
         }
     }
 }
