dcc25ba6f3d7122d2209d7a6a132be30931607a5
MGODOY <mgodoy_desenv@hotmail.com>
2017-04-09 23:34:03 -0300

    New filter: Painted by hand

---
+++ src/smartSpriteFX/smartSpriteFX.SpriteEffectModule/Effects/Filters/PaintedByHandFilter.cs
@@ -0,0 +1,133 @@
+﻿using smartSuite.smartSpriteFX.Effects.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using smartSuite.smartSpriteFX.Effects.Infra.UI.Configuratons;
+using smartSuite.smartSpriteFX.Pictures;
+using smartSuite.smartSpriteFX.Pictures.PixelPatterns;
+using smartSuite.smartSpriteFX.PictureEngine.Pictures;
+using System.Drawing;
+using smartSuite.smartSpriteFX.Pictures.ColorPattern;
+using smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters.UI;
+using smartSuite.smartSpriteFX.Effects.Infra;
+using smartSuite.smartSpriteFX.Effects.Core;
+
+namespace smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters
+{
+    /// <summary>
+    /// Represents a filter used to emulate painted by hand
+    /// </summary>
+    public class PaintedByHandFilter : SmartSpriteOriginalFilterBase, IContrastOrientedObject
+    {
+        /// <summary>
+        /// Gets or sets the contrast
+        /// </summary>
+        public int Contrast
+        {
+            get;
+            set;
+        }
+
+        public override bool ApplyFilter(Picture frame, int index)
+        {
+            List<PointInfo>[] columnList =
+                frame.GetColumns();
+
+            List<PointInfo>[] lineList =
+                frame.GetLines();
+
+            ColorEqualityComparer comparer = new ColorEqualityComparer();
+            ColorBuffer colorBuffer = new ColorBuffer(36, this.Contrast);
+
+            var transparentFilter =
+                EffectEngine.GetTransparentBackgroundFilter();
+
+            var cropFilter =
+                EffectEngine.FindFilter<CropFilter>();
+
+            Color transparentColor = Color.Transparent;
+            if (transparentFilter != null)
+            {
+                transparentColor = transparentFilter.TransparentColor;
+            }
+            if(cropFilter != null)
+            {
+                transparentColor = cropFilter.GetTransparentColor();
+            }
+
+            // Column handling
+            Color comparing = Color.Transparent;
+            foreach (var columnItem in columnList)
+            {
+                foreach (var pointItem in columnItem)
+                {
+                    #region Entries validation
+
+                    if (comparer.Equals(transparentColor, pointItem.Color))
+                    {
+                        continue;
+                    }
+
+                    #endregion
+
+                    Color currentColor =
+                        colorBuffer.GetSimilarColor(pointItem.Color);
+
+                    pointItem.Color = currentColor;
+                    comparing = currentColor;
+                }
+            }
+
+            // Line handling
+            comparing = Color.Transparent;
+            foreach (var lineItem in lineList)
+            {
+                foreach (var pointItem in lineItem)
+                {
+                    #region Entries validation
+
+                    if (comparer.Equals(transparentColor, pointItem.Color))
+                    {
+                        continue;
+                    }
+
+                    #endregion
+
+                    Color currentColor =
+                        colorBuffer.GetSimilarColor(pointItem.Color);
+
+                    pointItem.Color = currentColor;
+                    comparing = currentColor;
+                }
+            }
+
+            return true;
+        }
+
+        public override void Reset()
+        {
+            this.Contrast = 15;
+        }
+
+        public override IConfigurationPanel ShowConfigurationPanel()
+        {
+            return new ContrastConfigurationPanelControl();
+        }
+
+        /// <summary>
+        /// Gets the identification
+        /// </summary>
+        /// <returns></returns>
+        public override Identification GetIdentification()
+        {
+            var identification = base.GetIdentification();
+            identification.SetName("Painted By Hand");
+            identification.SetDescription("Represents a filter used to emulate paintings made by hand");
+            identification.SetGroup("Art");
+
+            return identification;
+        }
+    }
+}
