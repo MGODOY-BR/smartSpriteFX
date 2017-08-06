fc2d08d2cfc7443e9165fe5ea5e964f7e65c4fbb
MGodoy-BR <mgodoy_desenv@hotmail.com>
2017-08-06 12:32:45 -0300

    Implementação AlignToBottom

--- src/smartSpriteFX/smartSpriteFX.SpriteEffectModule/Effects/Filters/AlignToBottom.cs
+++ src/smartSpriteFX/smartSpriteFX.SpriteEffectModule/Effects/Filters/AlignToBottom.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using smartSuite.smartSpriteFX.Effects.Infra.UI.Configuratons;
 using smartSuite.smartSpriteFX.Pictures;
 using smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters.UI;
+using smartSuite.smartSpriteFX.Effects.Facade;
 
 namespace smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters
 {
@@ -17,7 +18,34 @@ namespace smartSuite.smartSpriteFX.SpriteEffectModule.Effects.Filters
     {
         public override bool ApplyFilter(Picture frame, int index)
         {
-            throw new NotImplementedException();
+            var cropFilter = EffectFacade.FindFilter<CropFilter>();
+            if(cropFilter == null)
+            {
+                cropFilter = new CropFilter();
+            }
+
+            if (!cropFilter.ApplyFilter(frame, index))
+            {
+                return false;
+            }
+            else
+            {
+                var lineList = frame.GetLines();
+                var lineRef = lineList[0];
+                var minY = lineRef.Min(p => p.Y);
+                var maxY = lineRef.Max(p => p.Y);
+                var yOffSetMin = Math.Abs(frame.OriginalHeight - minY);
+                var yOffSetMax = Math.Abs(frame.OriginalHeight - maxY);
+                var yOffSet = yOffSetMin + yOffSetMax;
+
+                frame.Height = frame.OriginalHeight;
+                foreach (var lineItem in lineList)
+                {
+                    lineItem.ForEach(l => l.Y = (yOffSetMax - l.Y));
+                }
+            }
+
+            return true;
         }
 
         public override void Reset()
